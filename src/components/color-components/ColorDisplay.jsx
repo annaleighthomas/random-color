3ed08ac037c760de09443fb14018bacf41ef08df
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-07-26 21:06:16 -0400

    ColorDisplay div cointainter

---
+++ src/components/color-components/ColorDisplay.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ColorDisplay = ({ backgroundColor }) => {
+  return (
+    <div
+      style={{
+        backgroundColor,
+        width: '100%',
+        height: '100%'
+      }}
+    ></div>
+  );
+};
+
+ColorDisplay.propTypes = {
+  backgroundColor: PropTypes.string.isRequired,
+};
+
+export default ColorDisplay;
