e18f9be84a350d6f6bcc73e11f07b8951275c15a
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-07-27 12:25:11 -0400

    update color function

--- src/containers/RandomColor.jsx
+++ src/containers/RandomColor.jsx
@@ -3,17 +3,28 @@ import ColorDisplay from '../components/color-components/ColorDisplay';
 
 export default class RandomColor extends Component {
   state = {
-    changingColor: 'blue'
+    changingColor: 'blue',
+    interval: ''
   };
 
+  updateColor() {
+    const color = ['blue', 'yellow', 'purple', 'orange', 'green'];
+    const randomNumber = Math.floor(Math.random() * color.length);
+    const assignColor = color[randomNumber];
+    this.setState({ changingColor: assignColor });
+  }
+
+  componentDidMount() {
+    setInterval(() => this.updateColor(), 1000);
+  }
+
+
   render() {
-    // const colors = ['blue', 'yellow', 'purple', 'orange', 'green'];
     const { changingColor } = this.state;
     return (
       <>
         <ColorDisplay backgroundColor={changingColor} />
       </>
-
     );
   }
 }
